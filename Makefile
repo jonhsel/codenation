f738ff382d64c53858beae10cf21069fdde43b02
Jonh Selmo <jonhselmo.engcomp@gmail.com>
2020-06-12 17:50:32 -0300

    arquivos

---
+++ Makefile
@@ -0,0 +1,13 @@
+build:
+	@python setup.py build
+
+sdist:
+	@python setup.py sdist
+
+install:sdist
+	@python setup.py install
+
+
+.PHONY: clean
+clean:
+	rm -fR build dist *egg-info
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "hello",
+    version = "0.0.1",
+    description = "Meu novo pacote",
+    author = "Jonh Selmo",
+    author_email = "jonhselmo.engcomp@gmail.com",
+    packages = ["hello"],
+    #install_requires = ["numpy"]
+)
